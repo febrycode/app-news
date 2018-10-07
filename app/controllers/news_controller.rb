c7782c53190dc4fd73a73ef9354ae6370a2f3f34
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-10-07 23:06:12 +0700

    implement update news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -14,4 +14,24 @@ class NewsController < ApplicationController
       render json: { error: news_service[:error], message: news_service[:message] }, status: 422
     end
   end
+
+  def update
+    begin
+      news = News.find(params[:id])
+
+      if news.update(news_params)
+        render json: { error: false, message: 'Data has been updated successfully' }, status: :ok
+      else
+        render json: { error: true, message: news.custom_full_messages }, status: 422
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: true, message: e.message }, status: 422
+    end
+  end
+
+  private
+
+  def news_params
+    params.permit(:name, :status)
+  end
 end
