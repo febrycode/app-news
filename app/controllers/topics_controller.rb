b75ba2cf81449a9b3afe6cabaac8a873c1f0aac8
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-10-07 16:18:59 +0700

    implement create topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,4 +4,20 @@ class TopicsController < ApplicationController
 
     render json: { error: false, topics: topics }, status: :ok
   end
+
+  def create
+    topic = Topic.new(topic_params)
+
+    if topic.save
+      render json: { error: false, message: 'Data has been created successfully' }, status: :created
+    else
+      render json: { error: true, message: topic.custom_full_messages }, status: 422
+    end
+  end
+
+  private
+
+  def topic_params
+    params.permit(:name)
+  end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,7 @@ class Topic < ApplicationRecord
   # Association
   has_many :news_topics
   has_many :news, through: :news_topics
+
+  # Validation
+  validates :name, presence: true
 end
